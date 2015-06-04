cb335788ec9c11fc0d419b09c7c9aff1815f7004
Doug Sheffer <doug.sheffer@researchsquare.com>
2015-06-04 14:01:06 -0400

    Split install

---
+++ install-part-1.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Copy wpa_supplicant information
+sudo cp ./etc/wpa.conf /etc/wpa_supplicant/wpa_supplicant.conf
+
+sudo reboot
--- install-part-2.sh
+++ install-part-2.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-# Copy wpa_supplicant information
-sudo cp ./etc/wpa.conf /etc/wpa_supplicant/wpa_supplicant.conf
-
 # Install PiTFT helper
 curl -SLs https://apt.adafruit.com/add | sudo bash
 sudo apt-get install -y raspberrypi-bootloader-adafruit-pitft
 sudo apt-get install -y adafruit-pitft-helper
-sudo adafruit-pitft-helper -t 28r
+sudo adafruit-pitft-helper -t 35r
 
 # Start the UI at boot
-cp ./etc/51-override-startup /etc/X11/Xsession.d/51-override-startup
+sudo cp ./etc/51-override-startup /etc/X11/Xsession.d/51-override-startup
+
+sudo reboot
